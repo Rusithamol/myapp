20d45bd09b7586e117c4a4b6c04ddfe22e299610
Rusithamol <152582269+Rusithamol@users.noreply.github.com>
2025-08-14 01:38:23 -0700

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
@@ -24,3 +25,4 @@ urlpatterns=[
     path('donar/',include('donar.urls')),
      path('',include('authentication.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
